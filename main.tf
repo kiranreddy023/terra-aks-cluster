5d951563ee7ae35e245ee15843dd54a7ce6f223d
KIRAN KUMAR V <109744725+kiranreddy023@users.noreply.github.com>
2023-09-12 22:36:07 +0530

    first commit

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "k8s-cluster" {
-  name = k8s-cluster
-  location = centralaustralia
+  name = "k8s-cluster"
+  location = "Central Australia"
 }
 
 resource "azurerm_kubernetes_cluster" "myk8scluster" {
-  name = myk8scluster
+  name = "myk8scluster"
   resource_group_name = azurerm_resource_group.k8s-cluster.name
   location = azurerm_resource_group.k8s-cluster.location
   dns_prefix = "kirank8s"
