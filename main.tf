a8f7a4a0c4344dc04b981873ab041908e0aa269e
KIRAN KUMAR V <109744725+kiranreddy023@users.noreply.github.com>
2023-09-12 22:30:08 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+resource "azurerm_resource_group" "k8s-cluster" {
+  name = k8s-cluster
+  location = centralaustralia
+}
+
+resource "azurerm_kubernetes_cluster" "myk8scluster" {
+  name = myk8scluster
+  resource_group_name = azurerm_resource_group.k8s-cluster.name
+  location = azurerm_resource_group.k8s-cluster.location
+  dns_prefix = "kirank8s"
+  default_node_pool {
+    name = "kirannodepool-dev"
+    node_count = 1
+    vm_size = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+  output "clientCertificate" {
+    value = "azurerm_kubernetes_cluster.myk8scluster.kube_config.0.client_certificate"
+    sensitive = true
+  }
+
+  output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+  sensitive = true
+  }
+
+}
